b64ed2261b29da4553efaef5cd76c165617a1da0
dadssi <dadssi.mohamed.abdelhak@gmail.com>
2025-01-15 09:33:28 +0000

    l'ajout de la classe Database

---
+++ classes/database/Classe-Database.php
@@ -0,0 +1,30 @@
+<?php
+class Database {
+    private static $instance = null;
+    private $connection;
+
+    private function __construct() {
+        try {
+            $this->connection = new PDO(
+                "mysql:host=localhost;dbname=udemy",
+                "username",
+                "password"
+            );
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die("connexion échouée : " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+}
+?>
